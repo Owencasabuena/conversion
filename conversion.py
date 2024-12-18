42012ffdac75ad073d99ed1832da4435cc7f5998
cyruz <cyruzzz16@gmail.com>
2024-12-18 17:52:30 +0800

    time & mass function added

--- conversion.py
+++ conversion.py
@@ -11,10 +11,10 @@ def convert(value, from_unit, to_unit):
     value = Decimal(value)
     """Convert a value from one metric unit to another."""
     try:
-        # Convert to base unit (meters)
+    
         value_in_meters = value * conversions[from_unit]
         
-        # Convert from base unit to target unit
+        
         converted_value = value_in_meters / conversions[to_unit]
         return round(converted_value, 3)
     except KeyError:
@@ -43,3 +43,26 @@ def convert_temp(value, from_unit, to_unit):
         raise ValueError("Invalid temperature units provided.")
     except ValueError as e:
         raise ValueError(f"Error in temperature conversion: {e}")
+
+def convert_time(value, from_unit, to_unit):
+    time_units = {
+        'seconds': 1,
+        'minutes': 60,
+        'hours': 3600,
+        'days': 86400,
+        'weeks': 604800,
+    }
+    value_in_seconds = value * time_units[from_unit]
+    result = value_in_seconds / time_units[to_unit]
+    return result
+
+def convert_mass(value, from_unit, to_unit):
+    mass_units = {
+        'milligram' : 1000,
+        'gram': 1,
+        'kilogram' : 0.001,
+    }
+    value_in_gram = value * mass_units[from_unit]
+    result = value_in_gram / mass_units[to_unit]
+    return result
+    
